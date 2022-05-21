8460d5fd85353d15160ce6b70f734e9d96215dcb
Vinny Silva <gomesvinny01@gmail.com>
2022-05-21 17:49:32 -0300

    Enviando projeto api

---
+++ api.js
@@ -0,0 +1,18 @@
+var inputElement = document.querySelector('#cep') 
+
+
+//function para pesquisar o cep
+function searchUser(){
+    axios.get(`https://viacep.com.br/ws/${inputElement.value}/json/`)
+    .then(res =>{
+        document.getElementById('rua').value = res.data.logradouro
+        document.getElementById('bairro').value = res.data.bairro
+        document.getElementById('cidade').value = res.data.localidade
+        document.getElementById('estado').value = res.data.uf
+
+    })
+    .catch(() => {
+        alert(`O cep ${inputElement.value} não existe!`);
+    })
+        
+};
